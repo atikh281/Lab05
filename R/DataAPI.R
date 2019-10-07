c33e1918f397ce1d3170406ac7e673ccba9c1738
José Jaime Méndez Flores <you@example.com>
2019-10-07 17:49:15 +0200

    Reduced code to a couple of functions

--- R/DataAPI.R
+++ R/DataAPI.R
@@ -13,33 +13,6 @@ regions <- function(){
   return(regions_are)
 }
 
-check_fuel_region <- function(region = "Scotland"){
-  #Make region to lowercase
-  region <- tolower(region)
-  #Check if region is valid
-  regions <- function(){ 
-    url <- "https://api.carbonintensity.org.uk/regional/"
-    regions_are <- jsonlite::fromJSON(url)
-    regions_are <- regions_are[["data"]][["regions"]][[1]][c(1,3)]
-    regions_are <- as.data.frame(regions_are)
-    regions_are$shortname <- tolower(regions_are$shortname)
-    return(regions_are)
-  }
-  regions <- regions()
-  stopifnot(region %in% regions$shortname)
-  region_id <- regions[1][regions[2] == region] 
-  #Make the new url
-  url <- paste("https://api.carbonintensity.org.uk/regional/regionid/", region_id, sep = "")
-  #Call the url and check http_status
-  response <- httr::GET(url)
-  stopifnot(httr::http_status(response)$category == "Success")
-  #Read JSON
-  resp_JSON <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"))
-  #Get Fuels
-  energy_fuel <- as.data.frame(resp_JSON[["data"]][["data"]][[1]][["generationmix"]])
-  return(energy_fuel)
-}
-
 check_fuel_region_time <- function(region = "Scotland", from = "now", to = "tomorrow"){
   stopifnot(is.character(region) || is.numeric(region), is.character(from), is.character(to))
   
@@ -123,19 +96,6 @@ check_fuel_region_time <- function(region = "Scotland", from = "now", to = "tomo
   return(energy_fuel)
   }
 
-pie_API <- function(region){
-  df <- check_fuel_region(region)
-    ggplot2::ggplot(data = df , ggplot2::aes(x ="" , y = perc , fill = fuel))+
-    ggplot2::geom_bar(stat = "identity", color = "white")+ 
-    ggplot2::coord_polar(theta = "y", start = 0)+
-    ggplot2::geom_text(ggplot2::aes(label = perc), position = ggplot2::position_stack(vjust = 0.55), size = 4 , check_overlap = TRUE)+
-    ggplot2::scale_fill_brewer(palette="Set1")+
-    ggplot2::theme_void()
-  
-}
-
-<<<<<<< HEAD
-=======
 pie_API_2 <- function(region, ...){
   df <- check_fuel_region_time(region, ...)
   ggplot2::ggplot(data = df , ggplot2::aes(x ="" , y = perc , fill = fuel))+
@@ -146,4 +106,3 @@ pie_API_2 <- function(region, ...){
     ggplot2::theme_void()
   
 }
->>>>>>> e55ec19109a3534254d286fdc00522c0c1c3cfe8
