866fb2c95aaa6974dc7f2b4bb399b314bf42c91c
José Jaime Méndez Flores <you@example.com>
2019-10-09 18:09:42 +0200

    Made changes for travis 1

---
+++ R/Team05Lab05.R
@@ -0,0 +1,8 @@
+#' @title A package for GETting information from https://carbonintensity.org.uk/.
+#' @description This package contains three functions \code{regions()}, \code{check_fuel_region_time()} and \code{pie_API_2()}
+#' @details This package calls data from the Carbon Intensity API and does some manipulations to organize the data and plot it in a pie chart. 
+#' 
+#'@references More information on Carbon Intensity UK API \href{https://carbonintensity.org.uk/}{here}.
+#'@name Team05Lab05
+#'@docType package
+"_PACKAGE"
