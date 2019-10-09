e2ef761d528821585d11f9baecf1b514a63200e0
José Jaime Méndez Flores <you@example.com>
2019-10-09 17:54:48 +0200

    Moved tests to folder

--- R/DataAPI.R
+++ R/DataAPI.R
@@ -127,20 +127,3 @@ pie_API_2 <- function(region, from, to){
     ggplot2::scale_fill_brewer(palette="Set1")+
     ggplot2::theme_void()
 }
- 
-
-test_that("Check that column 1 is Numeric", {
-  expect_equal(is.numeric(regions()$regionid), TRUE)
-})
-test_that("Check that column 2 is character", {
-  expect_equal(is.character(regions()$shortname), TRUE)
-})
-
-test_that("Check that sum of perc is 100", {
-  expect_equal(sum(check_fuel_region_time()$perc), 100)
-})
-
-test_that("Check the different kinds of fuel", {
-  expect_equal(check_fuel_region_time()$fuel,c("biomass","coal","imports","gas","nuclear","other","hydro","solar","wind"))
-})
-
